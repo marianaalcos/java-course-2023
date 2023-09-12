34379795401f4b38153c561282d2057d9d665dcf
balacmi <milos.balac@ivt.baug.ethz.ch>
2023-09-12 14:44:43 +0200

    Fixing a bug number 102

---
+++ src/main/java/java2023/day1/FirstExamp.java
@@ -0,0 +1,19 @@
+package java2023.day1;
+//new line
+public class FirstExamp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		
+		for (int i = 100; i < 10; i++) {
+			
+			// do something
+			
+		}
+		
+		//System.out.println(b);
+		
+	}
+
+}
